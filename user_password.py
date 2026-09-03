e1084a8818c2950aa3d5fdd9b7abdc04d327442f
Jisahab Patel <jisahabcode@gmail.com>
2026-09-03 11:04:05 +0530

    Add files via upload

---
+++ user_password.py
@@ -0,0 +1,19 @@
+user = input("Enter Your Name: ")
+
+if " " not in user and len(user) >= 5:
+
+    password = input("Enter Password: ")
+
+    if password == password.strip() and len(password) >= 8:
+        
+        if any(ch.isdigit() for ch in password):
+            print(f"Account Created Name: {user}")
+            print(f"Password: {password}")
+        else:
+            print("Password must contain a number")
+
+    else:
+        print("Invalid Password")
+
+else:
+    print("Invalid Username")
